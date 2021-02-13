a16d88758cdd3585f483e112ebe87ae16b83f831
Rajiv Srivastava <rajivmca2004@gmail.com>
2021-02-13 20:36:13 +0530

    Added Spring Cloud Sleuth depedencies

--- src/main/java/com/online/store/demo/controller/CatalogueController.java
+++ src/main/java/com/online/store/demo/controller/CatalogueController.java
@@ -1,6 +1,8 @@
 package com.online.store.demo.controller;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,6 +17,7 @@ import com.online.store.demo.repository.CatalogueRepository;
  */
 @RestController
 public class CatalogueController {
+	private static final Logger LOG = Logger.getLogger(CatalogueController.class.getName()); 
 	
 	@Autowired
 	private CatalogueRepository catalogueRepository;
@@ -23,6 +26,7 @@ public class CatalogueController {
     public Object fetchProducts ()
     {
         List<Catalogue> products = catalogueRepository.findAll();
+        LOG.log(Level.INFO, "you called home"); 
         return products;
     }
 
